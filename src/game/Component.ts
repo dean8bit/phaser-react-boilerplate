7ca5d3dc5376fede4fafd362d330633736455abf
Dean R <platoisalive@outlook.com>
2023-02-04 09:35:29 +0000

    update to function def

--- src/game/Component.ts
+++ src/game/Component.ts
@@ -1,14 +1,29 @@
 export default class Component {
 	public readonly gameObject: Phaser.GameObjects.GameObject;
 	private readonly componentReference: string;
-	constructor(gameobject: Phaser.GameObjects.GameObject, componentReference: string) {
+	constructor(
+		gameobject: Phaser.GameObjects.GameObject,
+		componentReference: string
+	) {
 		this.gameObject = gameobject;
 		this.componentReference = componentReference;
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		(this.gameObject as any)[this.componentReference] = this;
-		this.gameObject.scene.events.once(Phaser.Scenes.Events.UPDATE, this.tryOnStart, this);
-		this.gameObject.scene.events.on(Phaser.Scenes.Events.UPDATE, this.tryOnUpdate, this);
-		this.gameObject.on(Phaser.GameObjects.Events.DESTROY, this.tryOnDestroy, this);
+		this.gameObject.scene.events.once(
+			Phaser.Scenes.Events.UPDATE,
+			this.tryOnStart,
+			this
+		);
+		this.gameObject.scene.events.on(
+			Phaser.Scenes.Events.UPDATE,
+			this.tryOnUpdate,
+			this
+		);
+		this.gameObject.on(
+			Phaser.GameObjects.Events.DESTROY,
+			this.tryOnDestroy,
+			this
+		);
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -29,9 +44,21 @@ export default class Component {
 	public onDestroy(): void {}
 
 	private tryOnDestroy(): void {
-		this.gameObject.scene.events.off(Phaser.Scenes.Events.UPDATE, this.tryOnStart, this);
-		this.gameObject.scene.events.off(Phaser.Scenes.Events.UPDATE, this.tryOnUpdate, this);
-		this.gameObject.off(Phaser.GameObjects.Events.DESTROY, this.tryOnDestroy, this);
+		this.gameObject.scene.events.off(
+			Phaser.Scenes.Events.UPDATE,
+			this.tryOnStart,
+			this
+		);
+		this.gameObject.scene.events.off(
+			Phaser.Scenes.Events.UPDATE,
+			this.tryOnUpdate,
+			this
+		);
+		this.gameObject.off(
+			Phaser.GameObjects.Events.DESTROY,
+			this.tryOnDestroy,
+			this
+		);
 		this.tryCall(this.onDestroy);
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		delete (this.gameObject as any)[this.componentReference];
@@ -42,12 +69,19 @@ export default class Component {
 	}
 
 	// eslint-disable-next-line @typescript-eslint/ban-types
-	private tryCall(f: Function) {
+	private tryCall(f: () => void) {
 		try {
 			f.bind(this)();
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} catch (e: any) {
-			console.log("(" + this.gameObject.name + ":" + this.componentReference + ") Error: " + e.stack);
+			console.log(
+				"(" +
+					this.gameObject.name +
+					":" +
+					this.componentReference +
+					") Error: " +
+					e.stack
+			);
 		}
 	}
 }
