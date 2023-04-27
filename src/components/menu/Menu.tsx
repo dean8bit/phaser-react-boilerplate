4c96b277d597e52c8cc70816b6a134f58ee56890
Dean R <platoisalive@outlook.com>
2023-04-27 12:27:03 +0000

    added event to wait for phaser scene to load

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,13 +1,17 @@
 import "./menu.css";
 
-import React from "react";
+import React, { useState } from "react";
 import { useGameContext } from "../app";
 
 const Menu: React.FC = () => {
 	const { game } = useGameContext();
+	const [ready, setReady] = useState(false);
+
+	game?.events.on("ready", () => setReady(true));
+
 	return (
 		<div className="bit-container menu">
-			<div>Menu</div>
+			<div>Menu {game?.scene.getScene("GameScene")?.scene.key}</div>
 		</div>
 	);
 };
