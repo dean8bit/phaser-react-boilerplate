b2a426cf72593e53913868830fce816c2d169018
Dean R <platoisalive@outlook.com>
2023-04-27 12:12:51 +0000

    simplified code

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,12 +1,10 @@
 import "./menu.css";
 
 import React from "react";
-import * as Scenes from "../../game/scenes";
 import { useGameContext } from "../app";
 
 const Menu: React.FC = () => {
 	const { game } = useGameContext();
-	const scene = game?.scene.getScene("GameScene") as Scenes.GameScene;
 	return (
 		<div className="bit-container menu">
 			<div>Menu</div>
